e449920a82932e7763dcc3c97cac24482c023bc6
nschaller <nschaller@student.ethz.ch>
2026-03-06 17:50:22 +0100

    test

--- src/colors/error.cpp
+++ src/colors/error.cpp
@@ -1,6 +1,12 @@
 #include "colored_io.h"
 
-int main(int argc, char* argv[]) {
+int 
+
+
+
+
+
+main(int argc, char* argv[]) {
     if (argc < 2) {
         ERROR("Usage: " + std::string(argv[0]) + " <message>");
         return 1;
