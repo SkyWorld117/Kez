12f291cd0babf4477fe1dbc303b2c3e690580d17
SkyWorld117 <alanzhu02@gmail.com>
2026-04-05 22:46:42 +0200

    Improve generator logic

--- src/user_config_generator/user_config_generator.cpp
+++ src/user_config_generator/user_config_generator.cpp
@@ -1,7 +1,8 @@
 #include <user_config_generator/user_config_generator.hpp>
 
 void config_per_pkg(YAML::Node& config, const YAML::Node& db_pkg_node,
-                    const YAML::Node& abstract_packages) {
+                    const std::vector<std::string>& all_dependencies,
+                    const std::vector<std::string>& target_packages) {
     std::string pkg_name = db_pkg_node["cheese"]["name"].as<std::string>();
 
     config["cheese"][pkg_name] = YAML::Node(YAML::NodeType::Map);
@@ -19,17 +20,16 @@ void config_per_pkg(YAML::Node& config, const YAML::Node& db_pkg_node,
         config["cheese"][pkg_name]["compiler"] = global_config::get_default_compiler();
     }
     // MPI needs to be handled separately
-    std::vector<std::string> all_dependencies =
-        config["recipe"]["dependencies"].as<std::vector<std::string>>();
     bool pkg_is_target = (db_pkg_node["cheese"]["type"].as<std::string>() != "mpi" &&
                           db_pkg_node["cheese"]["type"].as<std::string>() != "compiler") ||
-                         all_dependencies[0] == pkg_name;
+                         (std::find(target_packages.begin(), target_packages.end(), pkg_name) !=
+                          target_packages.end());
     if (db_pkg_node["cheese"]["build"] && pkg_is_target) {
         if (db_pkg_node["cheese"]["build"]["configurations"]) {
             if (db_pkg_node["cheese"]["build"]["configurations"]) {
-                YAML::Node filtered_config =
-                    filtered_configurations(db_pkg_node["cheese"]["build"]["configurations"],
-                                            all_dependencies, abstract_packages);
+                YAML::Node filtered_config = filtered_configurations(
+                    db_pkg_node["cheese"]["build"]["configurations"], all_dependencies,
+                    config["recipe"]["abstract_packages"]);
                 if (!filtered_config.IsNull() && !(filtered_config.size() == 0)) {
                     if (!config["cheese"][pkg_name]["build"]) {
                         config["cheese"][pkg_name]["build"] = YAML::Node(YAML::NodeType::Map);
@@ -39,8 +39,9 @@ void config_per_pkg(YAML::Node& config, const YAML::Node& db_pkg_node,
             }
         }
         if (db_pkg_node["cheese"]["build"]["stages"]) {
-            YAML::Node filtered_stages_node = filtered_stages(
-                db_pkg_node["cheese"]["build"]["stages"], all_dependencies, abstract_packages);
+            YAML::Node filtered_stages_node =
+                filtered_stages(db_pkg_node["cheese"]["build"]["stages"], all_dependencies,
+                                config["recipe"]["abstract_packages"]);
             if (!filtered_stages_node.IsNull() && !(filtered_stages_node.size() == 0)) {
                 if (!config["cheese"][pkg_name]["build"]) {
                     config["cheese"][pkg_name]["build"] = YAML::Node(YAML::NodeType::Map);
@@ -92,7 +93,7 @@ YAML::Node gen_user_config(const std::vector<std::string>& pkg_name, bool intera
     std::filesystem::path db_path(getenv("FROMAGER_DB"));
     for (const auto& dep : dependencies) {
         YAML::Node db_pkg_node = get_db_config(dep);
-        config_per_pkg(config, db_pkg_node, config["recipe"]["abstract_packages"]);
+        config_per_pkg(config, db_pkg_node, all_dependencies, pkg_name);
     }
 
     return config;
