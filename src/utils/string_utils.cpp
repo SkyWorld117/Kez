a29881bd2c29153040b0ed4d339870decbbb7b3d
Sophia Herrmann <sophia.herrmann@inf.ethz.ch>
2026-04-25 16:04:15 +0200

    add split() function

---
+++ src/utils/string_utils.cpp
@@ -0,0 +1,20 @@
+#include <utils/string_utils.hpp>
+
+std::vector<std::string> split(const std::string& str, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    for (char ch : str) {
+        if (ch == delimiter) {
+            if (!token.empty()) {
+                tokens.push_back(token);
+                token.clear();
+            }
+        } else {
+            token += ch;
+        }
+    }
+    if (!token.empty()) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
