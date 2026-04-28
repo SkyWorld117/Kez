6716a7a1059e141fd6f46efecc89dc73a8dcd3c0
SkyWorld117 <alanzhu02@gmail.com>
2026-04-28 10:35:19 +0200

    Improve backend efficiency

--- include/database/database.hpp
+++ include/database/database.hpp
@@ -2,11 +2,11 @@
 
 #include <yaml-cpp/yaml.h>
 
-#include <utils/colored_io.hpp>
 #include <filesystem>
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <utils/colored_io.hpp>
 #include <vector>
 
-YAML::Node get_db_config(std::string pkg_name);
+YAML::Node get_db_config(const std::string& pkg_name);
