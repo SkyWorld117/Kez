1ac89854360780566f5124e531c04b744b9b310d
SkyWorld117 <alanzhu02@gmail.com>
2026-05-18 15:03:20 +0200

    Implement version parsing and use it for gcc

--- include/parser/conditions_parser.hpp
+++ include/parser/conditions_parser.hpp
@@ -8,4 +8,4 @@
 #include <utils/colored_io.hpp>
 
 std::string parse_conditions(const std::string& base_value, const YAML::Node& conditions_node,
-                             const ParserContext& context);
+                             ParserContext& context);
--- include/utils/string_utils.hpp
+++ include/utils/string_utils.hpp
@@ -7,6 +7,14 @@
 #include "dump.hpp"
 
 std::vector<std::string> split(const std::string& str, char delimiter);
+std::vector<std::string> split(const std::string& str, const std::vector<char>& delimiters);
+std::vector<std::string> split_keep_delimiters(const std::string& str,
+                                               const std::vector<char>& delimiters);
+
+bool is_alphabetic(const std::string& str);
+bool is_alphabetic(char c);
+bool is_numeric(const std::string& str);
+bool is_numeric(char c);
 
 std::size_t get_length_without_color(const std::string& str);
 
